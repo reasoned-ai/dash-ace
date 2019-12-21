7f0a087ba7a9529fee23627e70b83112af5a1eba
xumiao <pacificxumiao@gmail.com>
2019-12-20 16:39:12 -0800

    package init setup

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import json
-import os
 from setuptools import setup
 
 
@@ -8,16 +7,31 @@ with open('package.json') as f:
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
+long_description = """
+# dash ace
+
+dash ace is a Dash component for Ace Editor.
+
+```
+    $ pip install dash-ace
+```
+"""
+
 setup(
     name=package_name,
     version=package["version"],
     author=package['author'],
+    author_email="xu@reasoned.ai",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6.0',
+    url=package['homepage'],
     packages=[package_name],
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
     install_requires=[],
-    classifiers = [
+    classifiers=[
         'Framework :: Dash',
     ],    
 )
